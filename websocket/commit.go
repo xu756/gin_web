d10e2c69587f3b682348a8a46ab6c61dceead598
xu756 <756334744@qq.com>
2022-05-28 00:39:07 +0800

    配置websocket

---
+++ websocket/commit.go
@@ -0,0 +1,16 @@
+package websocket
+
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     checkOrigin,
+}
+
+func checkOrigin(r *http.Request) bool {
+	return true
+}

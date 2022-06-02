14a0fd7e07e5e285444535ff4d69096468d78fee
xu756 <756334744@qq.com>
2022-06-02 21:25:18 +0800

    封装router

--- router/user.go
+++ router/user.go
@@ -8,10 +8,11 @@ import (
 // UserRouter 用户模块
 func UserRouter(r *gin.Engine) *gin.Engine {
 	rr := r.Group("/api/user")
-	rr.GET("/", views.Index)                        // 首页
-	rr.POST("/login", views.UserLogin)              // 登录
-	rr.POST("/register", views.UserRegister)        // 注册
-	rr.GET("/userinfo/user=:token", views.UserInfo) // 获取用户信息
-	rr.GET("/portrait", views.UploadPortrait)
+	rr.GET("/", views.Index)                                           // 首页
+	rr.POST("/login", views.UserLogin)                                 // 登录
+	rr.POST("/register", views.UserRegister)                           // 注册
+	rr.GET("/userinfo/user=:token", views.UserInfo)                    // 获取用户信息
+	rr.GET("/set/portrait", views.UploadPortrait)                      // 上传头像
+	rr.StaticFS("/get/portrait", gin.Dir("./media/upload/user", true)) // 获取头像
 	return r
 }

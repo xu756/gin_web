7dec9683f1d1e2593ed7ae592e4cd405a7141222
xu756 <756334744@qq.com>
2022-06-12 09:46:44 +0800

    404错误

--- router/router.go
+++ router/router.go
@@ -6,7 +6,15 @@ import (
 )
 
 func InitRouter() {
-	r := gin.Default()    // 初始化路由
+	r := gin.Default() // 初始化路由
+	//404
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"code": 404,
+			"msg":  "请求路径不存在",
+		})
+
+	})
 	UserRouter(r)         // 用户模块
 	err := r.Run(":7000") // 监听端口
 	fmt.Println("启动成功")
